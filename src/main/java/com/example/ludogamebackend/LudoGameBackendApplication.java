e7caa27ff75b4c773784303d9f82c09e0b035f86
Suad Muhic <105297253+SuadMuhic@users.noreply.github.com>
2023-08-31 10:44:52 +0200

    first commit

---
+++ src/main/java/com/example/ludogamebackend/LudoGameBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.ludogamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoGameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LudoGameBackendApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/ludogamebackend/LudoGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ludogamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoGameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
